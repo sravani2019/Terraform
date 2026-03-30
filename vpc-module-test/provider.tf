0dc7d9cc1bf6892b58db9465f4d89f1e0b900b05
sravani192019 <sravani.jamithireddy@gmail.com>
2026-03-31 03:22:11 +0530

    vpc

---
+++ vpc-module-test/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.32.1"
+    }
+  }
+}
+terraform {
+  backend "s3" {
+    bucket = "84s-devops-state"
+    key    = "vpc_test"
+    region = "us-east-1"
+    #dynamodb_table = "84s-devops-state"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+}
